29f73997b777c6a571325360cf9cf33d38f624e6
leandro-henrique09 <leandrohdc09@gmail.com>
2025-12-31 15:40:21 -0300

    exercicios e aulas

---
+++ exercicios/09_where.sql
@@ -0,0 +1,11 @@
+-- Listar todas as transações adicionando uma coluna nova sinalizando “alto”, “médio” e “baixo”,
+-- para o valor dos pontos [<10 ; <500; >=500]
+SELECT QtdePontos,
+
+       CASE
+            WHEN QtdePontos < 10 THEN 'Baixo'
+            WHEN QtdePontos < 500 THEN 'Medio'
+            ELSE 'Alto'
+        END AS 'Classificação' 
+FROM transacoes
+
